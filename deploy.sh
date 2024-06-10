9396b49ca04ade78cbaa3011c4488414ff6f6adf
Chris Young <chris@unsatisfactorysoftware.co.uk>
2024-06-11 00:12:38 +0100

    Set project and region

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-gcloud config set project fnic-2024
-gcloud run jobs deploy bestofbot-fnic --source . --set-env-vars=BOTUSER="best_of_fnic_bot",BOTINSTANCE="lemmy.dbzer0.com",COMMUNITY="bot_playground_2@lemmings.world",COMMLIST="_fnic_comms.json" --set-secrets="BOTPW=best_of_fnic_bot:latest"
+gcloud run jobs deploy bestofbot-fnic --project=fnic-2024 --region=europe-west1 --source . --set-env-vars=BOTUSER="best_of_fnic_bot",BOTINSTANCE="lemmy.dbzer0.com",COMMUNITY="bot_playground_2@lemmings.world",COMMLIST="_fnic_comms.json" --set-secrets="BOTPW=best_of_fnic_bot:latest"
