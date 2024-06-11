29dabebff7f7fa73405eb5c981b63d4b18c52e10
Chris Young <chris@unsatisfactorysoftware.co.uk>
2024-06-11 09:56:27 +0100

    Live community

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud run jobs deploy bestofbot-fnic --project=fnic-2024 --region=europe-west1 --source . --set-env-vars=BOTUSER="best_of_fnic_bot",BOTINSTANCE="lemmy.dbzer0.com",COMMUNITY="bot_playground_2@lemmings.world",COMMLIST="_fnic_comms.json",POSTTITLE="Top Imaginary Network posts last week" --set-secrets="BOTPW=best_of_fnic_bot:latest"
+gcloud run jobs deploy bestofbot-fnic --project=fnic-2024 --region=europe-west1 --source . --set-env-vars=BOTUSER="best_of_fnic_bot",BOTINSTANCE="lemmy.dbzer0.com",COMMUNITY="lemmydirectory@lemmy.dbzer0.com",COMMLIST="_fnic_comms.json",POSTTITLE="Top Imaginary Network posts last week" --set-secrets="BOTPW=best_of_fnic_bot:latest"
