d9540238b045e385d9485aa0d4ca09699735c3f0
M4rtineski <137183342+M4rtineski@users.noreply.github.com>
2024-06-12 13:19:25 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud run jobs deploy bestofbot-fnic --project=fnic-2024 --region=europe-west1 --source . --set-env-vars=BOTUSER="best_of_fnic_bot",BOTINSTANCE="lemmy.dbzer0.com",COMMUNITY="lemmydirectory@lemmy.dbzer0.com",COMMLIST="_fnic_comms.json",POSTTITLE="Top Imaginary Network posts last week" --set-secrets="BOTPW=best_of_fnic_bot:latest"
+gcloud run jobs deploy bestofbot-fnic --project=fnic-2024 --region=europe-west1 --source . --set-env-vars=BOTUSER="best_of_fnic_bot",BOTINSTANCE="lemmy.dbzer0.com",COMMUNITY="lemmydirectory@lemmy.dbzer0.com",COMMLIST="_fnic_comms.json",POSTTITLE="💭 ❗Top Imaginary Network posts of the week❗ 💭" --set-secrets="BOTPW=best_of_fnic_bot:latest"
