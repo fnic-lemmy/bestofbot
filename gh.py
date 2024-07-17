126c03aecb5371063d733420af154d63f5ef830e
Chris Young <chris@unsatisfactorysoftware.co.uk>
2024-07-17 22:07:05 +0100

    Put get_gist in try..except block
    See #46

--- gh.py
+++ gh.py
@@ -7,7 +7,11 @@ def gist_update(filename, contents, gist, auth):
   if gist != 0:
     auth = Auth.Token(auth)
     g = Github(auth=auth)
-    gist = g.get_gist(gist)
+    try:
+      gist = g.get_gist(gist)
+    except Exception as e:
+      print(f'Cannot get gist: {e}')
+      return
 
     try:
       gist.edit(files = {f'{filename}': InputFileContent(content=contents)})
