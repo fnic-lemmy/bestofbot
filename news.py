2e2628f05efacc736dbbaf8788ebaecacc394bf8
Chris Young <chris@unsatisfactorysoftware.co.uk>
2024-08-14 16:12:24 +0100

    Set user-agent for newspaper

--- news.py
+++ news.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 from newspaper import Article
+from newspaper import Config
 
 def article_image(url):
+  config = Config()
+  config.browser_user_agent = "Mozilla/5.0"
+
   article = Article(url)
   try:
     article.download()
   except Exception as e:
+    print(f'unable to download article, {e}')
     return None
   article.parse()
   if article.top_image is not None:
