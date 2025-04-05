9693050666ac24b2c96716e6449e77fb67b26231
Chris Young <chris@unsatisfactorysoftware.co.uk>
2025-04-05 15:11:39 +0100

    try invidious

--- yt.py
+++ yt.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-from pytubefix import YouTube
+from invidious.enums import ContentType
+from invidious import *
 import urllib.parse as urlparse
 from urllib.parse import parse_qs
 import shorten
 import ssl
 
 def get(url, rapidkey):
-  proxy = {
-    "http": "socks5://99.80.11.54:80",
-    "https": "socks5://99.80.11.54:80"
-  }
-
-  yt = None
+  parsed = urlparse.urlparse(url)
+  qs = parse_qs(parsed.query)
 
   if (url[:17] != 'https://youtu.be/') and (url[:20] != 'https://youtube.com/') and (url[:24] != 'https://www.youtube.com/'):
-    parsed = urlparse.urlparse(url)
-    if 'v' not in parse_qs(parsed.query):
+    if 'v' not in qs:
       return None
 
   try:
@@ -25,21 +21,38 @@ def get(url, rapidkey):
   else:
     ssl._create_default_https_context = _create_unverified_https_context
 
+  if 'v' in qs:
+    id = qs['v']
+
+  if parsed.netloc == 'youtu.be':
+    id = parsed.path[1:]
+
   try:
-    yt = YouTube(url, proxies=proxy)
+    iv = Invidious(mirrors = ['https://invidious.nerdvpn.de'])
   except Exception as e:
     print({e})
     return None
 
-  t = ''
+  print(id)
 
-  if yt.thumbnail_url is not None:
-    t = f'![]({yt.thumbnail_url})\n\n'
-  if yt.title is not None:
-    t += f'*{yt.title}*\n\n'
-  else:
-    print('no title')
-    return None
-  if yt.description is not None:
-    t += shorten.shorten_text(yt.description, rapidkey)
-  return t
+  vid = iv.get_video(video_id = id, region = 'GB')
+  print(vid)
+
+  l = iv.search(id)
+  print(l)
+
+  #t = ''
+
+  #if yt.thumbnail_url is not None:
+  #  t = f'![]({yt.thumbnail_url})\n\n'
+  #if yt.title is not None:
+  #  t += f'*{yt.title}*\n\n'
+  #else:
+  #  print('no title')
+  #  return None
+  #if yt.description is not None:
+  #  t += shorten.shorten_text(yt.description, rapidkey)
+  #return t
+
+get('https://youtu.be/T_Ig2rqhK28?si=SSY7m5gnEKYPU2Yn', None)
+
